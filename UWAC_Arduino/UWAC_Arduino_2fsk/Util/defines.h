1288358945748eac0ae5b00c76c971dee4f13801
mrkefskie <mrkefskie@gmail.com>
2016-04-01 20:39:55 +0200

    Added code for 2fsk, which can use more characters than 60

---
+++ UWAC_Arduino/UWAC_Arduino_2fsk/Util/defines.h
@@ -0,0 +1,31 @@
+/*
+ * defines.h
+ *
+ * Created: 10-3-2016 08:45:15
+ *  Author: Kevin Slagmolen
+ */ 
+
+
+#ifndef DEFINES_H_
+#define DEFINES_H_
+
+#define DEBUG
+
+#define F_CPU			16000000UL
+
+/* START USART DEFINES */
+#define USART_BAUD		921600
+#define UBRR_BAUDRATE	(((F_CPU / (USART_BAUD * 16UL))) - 1)
+/* END USART DEFINES */
+
+/* START I2C DEFINES*/
+#define F_SCL			100000UL
+#define Prescaler		1
+#define TWBR_val		((((F_CPU / F_SCL) / Prescaler) - 16) / 2)
+/* END I2C DEFINES */
+
+#define BAUDRATE		500
+
+#define POLY			0x167
+
+#endif /* DEFINES_H_ */
--- UWAC_Arduino/UWAC_Arduino_2fsk/main.c
+++ UWAC_Arduino/UWAC_Arduino_2fsk/main.c
@@ -1,11 +1,12 @@
 /*
- * UWAC_Arduino.c
+ * UWAC_Arduino_2fsk.c
  *
- * Created: 10-3-2016 08:44:13
+ * Created: 1-4-2016 19:06:22
  * Author : Kevin Slagmolen
  */ 
 
-#include "Util/defines.h"
+ 
+ #include "Util/defines.h"
 
 #include <avr/io.h>
 #include <util/delay.h>
@@ -14,38 +15,71 @@
 #include "Util/I2C.h"
 #include "data_conversion.h"
 
+void sendData(int* dataToSend); // Test function to see if the data is put on the output pin (pin 13)
+
 FILE usart0_output = FDEV_SETUP_STREAM(USART0SendByte, NULL, _FDEV_SETUP_WRITE);	// Output file for USART
 FILE usart0_input = FDEV_SETUP_STREAM(NULL, USART0GetByte, _FDEV_SETUP_READ);		// Input file for USART
 
 int main(void)
 {
+	DDRB |= (1 << DDB5);
+
+	PORTB |= (1 << DDB5);
+
 	/* USART INIT START */
 	USART0Init();
 	stdout =& usart0_output;
 	stdin =& usart0_input;
 	/* USART INIT STOP */
 
-	i2c_init(); // Init I2C (TWI)
+	i2c_init(); // Init I2C (Two Wire Interface)
 
 	//char input[55] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"; // MAX STRING LENGTH BEFORE UC DOES WIERD STUFF
+	//char input[55] = "test";
 
 	char input[100] = "Now, this is a story all about how My life got flipped-turned upside down";
 
 	#ifdef DEBUG
 	printf("\n\n\n\nSTART CODE WITH THIS INPUT: %s\n\n", input); // Print the input string
 	#endif // DEBUG
-
+		
 	int* dataToSend = fillDataArray(input); // Convert data to base4
 	char* received = convertToData(dataToSend, dataLength); // Convert base4 to data
 
 	#ifdef DEBUG
 	printf("\nReceived data: %s\n\n", received); // Print the received data
 
+	unsigned int time;
+
+	time = (1000 / BAUDRATE) * dataLength;
+
+	printf("Baudtime: %i\n", 1000/BAUDRATE);
+	printf("Time needed to send: %i ms\n", time);
+
+	PORTB &= ~(1 << DDB5);
+
+	sendData(dataToSend);
+
 	printf("Entering loop now....\n");
 	#endif
 
-    while (1) 
-    {
+	while (1)
+	{
 		// Main loop
-    }
+	}
+}
+
+void sendData(int* dataToSend)
+{
+	int i;
+
+	for (i = 0; i < dataLength; i++)
+	{
+		if (dataToSend[i] == 0)
+			DDRB &= ~(1 << DDB5);
+		else
+			DDRB |= (1 << DDB5);
+
+		_delay_ms(1000/BAUDRATE);
+	}
 }
